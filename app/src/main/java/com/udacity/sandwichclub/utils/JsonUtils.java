b9d51c0aca902540bd608b3af38c9654400b50b2
prateekgarg <garg.prateek1@gmail.com>
2018-05-05 17:47:01 -0700

    Completed Json parsing util and added it to sandwich object using set methods

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,56 @@
 package com.udacity.sandwichclub.utils;
 
+import android.content.Context;
+
+import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
+
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+
+            Sandwich sandwich = new Sandwich();
+
+            JSONObject name = jsonObject.getJSONObject("name");
+            sandwich.setMainName(name.getString("mainName"));
+
+
+            JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAsStringList = new ArrayList<>();
+            for (int i = 0; i < alsoKnownAsArray.length(); i++) {
+                alsoKnownAsStringList.add(alsoKnownAsArray.getJSONObject(i).toString());
+            }
+            sandwich.setAlsoKnownAs(alsoKnownAsStringList);
+
+
+            sandwich.setDescription(jsonObject.getString("description"));
+            sandwich.setImage(jsonObject.getString("image"));
+
+            JSONArray ingredientsArray = jsonObject.getJSONArray("ingredients");
+            List<String> ingredientsList = new ArrayList<>();
+            for (int i = 0; i < ingredientsArray.length(); i++) {
+                ingredientsList.add(ingredientsArray.getString(i));
+            }
+            sandwich.setIngredients(ingredientsList);
+
+            sandwich.setPlaceOfOrigin(jsonObject.getString("placeOfOrigin"));
+
+            return sandwich;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
